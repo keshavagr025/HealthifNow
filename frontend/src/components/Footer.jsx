03e012eaa55a6507827e120b5ffec18f512d14a6
khushii-agrawal <khushipremy789@gmail.com>
2025-03-23 18:49:53 +0530

    update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
         </div>
 
         <div>
-          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH ON</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
             <li>+1-212-456-7890</li>
             <li>greatstackdev@gmail.com</li>
